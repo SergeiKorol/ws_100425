0ee79487428db0f68c9116d7fbda1f4ccde6a5b2
SergeiKorol <124100194+SergeiKorol@users.noreply.github.com>
2025-04-10 20:15:14 +0300

    Коммит№2

--- test/add_task_test.py
+++ test/add_task_test.py
@@ -5,6 +5,6 @@ def test_add():
     response = requests.post(url="https://todo-app-sky.herokuapp.com/", json=body)
     response_body = response.json()
     
+    # ПРоверяем статус 
     assert response.status_code == 202
     assert response_body['completed'] == False
-    
